0f3df6c747b31f74c325f1ab5457b61bdf0815c0
kotpilun <kotpilun3m@gmail.com>
2024-03-05 09:41:44 +0300

    part 1

---
+++ app.py
@@ -0,0 +1,22 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+bot = Bot(token='6958116066:AAEY2WroSKhre3gC_u8Z9w7TUvOj01BeXrA')
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def start_cmd(message:types.Message):
+    await message.answer('Это была команда старт')
+
+@dp.message()
+async def echo(message:types.Message):
+    await message.answer(message.text)
+
+
+
+async def main():
+    await dp.start_polling(bot);
+
+asyncio.run(main())
