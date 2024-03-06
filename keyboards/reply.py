16c72fdc650eed4e04241815d31ff8d07087de29
kotpilun <kotpilun3m@gmail.com>
2024-03-06 16:54:13 +0300

    part 6

--- keyboards/reply.py
+++ keyboards/reply.py
@@ -1,32 +1,39 @@
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from aiogram.types import KeyboardButton
 from aiogram.utils.keyboard import ReplyKeyboardBuilder
 
-start_kb = ReplyKeyboardMarkup(
-    keyboard=[
-        [
-            KeyboardButton(text="Menu"),
-            KeyboardButton(text="About"),
-        ],
-        [
-            KeyboardButton(text="Payment"),
-            KeyboardButton(text="Shipping"),
-        ],
-    ],
-    resize_keyboard=True,
-    input_field_placeholder='What do you want?'
-)
 
-del_keyboard = ReplyKeyboardRemove()
+def get_keyboard(
+    *btns: str,
+    placeholder: str = None,
+    request_contact: int = None,
+    request_location: int = None,
+    sizes: tuple[int] = (2,),
+):
+    '''
+    Parameters request_contact and request_location must be as indexes of btns args for buttons you need.
+    Example:
+    get_keyboard(
+            "Меню",
+            "О магазине",
+            "Варианты оплаты",
+            "Варианты доставки",
+            "Отправить номер телефона",
+            placeholder="Что вас интересует?",
+            request_contact=4,
+            sizes=(2, 2, 1)
+        )
+    '''
+    keyboard = ReplyKeyboardBuilder()
 
-start_kb2 = ReplyKeyboardBuilder()
-start_kb2.add(
-    KeyboardButton(text="Menu"),
-    KeyboardButton(text="About"),
-    KeyboardButton(text="Payment"),
-    KeyboardButton(text="Shipping"),
-)
-start_kb2.adjust(2, 2)
+    for index, text in enumerate(btns, start=0):
+        
+        if request_contact and request_contact == index:
+            keyboard.add(KeyboardButton(text=text, request_contact=True))
 
-start_kb3 = ReplyKeyboardBuilder()
-start_kb3.attach(start_kb2)
-start_kb3.row(KeyboardButton(text="Contact"))
+        elif request_location and request_location == index:
+            keyboard.add(KeyboardButton(text=text, request_location=True))
+        else:
+            keyboard.add(KeyboardButton(text=text))
+
+    return keyboard.adjust(*sizes).as_markup(
+            resize_keyboard=True, input_field_placeholder=placeholder)
